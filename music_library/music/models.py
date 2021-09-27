0b9ab3d74052228c3fe873f240c04a8f96e0a80e
Megan Cindric <megancindric@gmail.com>
2021-09-27 09:58:41 -0500

    Project setup & init

---
+++ music_library/music/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length = 20)
+    artist = models.CharField(max_length = 20)
+    album = models.CharField(max_length = 20)
+    release_date = models.DateField()
+    genre = models.CharField(max_length = 20)
