c14043ce46d187aa187f64ad2e29ea91ec3c22a6
Megan Cindric <megancindric@gmail.com>
2021-09-27 10:17:45 -0500

    Creating song serializer

---
+++ music_library/music/serializer.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from music.models import Song
+
+
+class SongSerializer(serializers.Serializer):
+    title = serializers.CharField(required=True, allow_blank = False)
+    artist = serializers.CharField(required=True, allow_blank = False)
+    album = serializers.CharField(required=True,allow_bank=False)
+    release_date = serializers.DateField()
+    genre = serializers.CharField(required=True,allow_blank=False)
+
+
+    def create(self, validated_data):
+        return Song.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.artist = validated_data.get('artist', instance.artist)
+        instance.album = validated_data.get('album', instance.album)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.genre = validated_data.get('genre', instance.genre)
+        instance.save()
+        return instance
+
